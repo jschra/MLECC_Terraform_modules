3c90a8ce42e884567ba954e94574f59c6823ccd5
Jorik Schra <jschra@deloitte.nl>
2022-02-23 13:43:33 +0100

    Clean up 2

--- eredivisie-app/outputs.tf
+++ eredivisie-app/outputs.tf
@@ -1,12 +1,10 @@
 output "lambda_bucket_name" {
   description = "Name of the S3 bucket used to store function code."
-
   value = aws_s3_bucket.lambda-bucket.id
 }
 
 output "function_name" {
   description = "Name of the Lambda function."
-
   value = aws_lambda_function.api.function_name
 }
 
